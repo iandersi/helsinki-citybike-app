109ea7743b11244887a9b26014ae28684505d216
Ina <iandersin@gmail.com>
2023-05-04 19:33:22 +0300

    Edited .gitignore, made script file, edited dataImport

--- backend/db/0_preparedData.sh
+++ backend/db/0_preparedData.sh
@@ -1,1 +1,4 @@
-echo "hello"
+tail -n +2 /docker-entrypoint-initdb.d/2021-05.csv | tac > /docker-entrypoint-initdb.d/2021-05-prepared.csv
+tail -n +2 /docker-entrypoint-initdb.d/2021-06.csv | tac > /docker-entrypoint-initdb.d/2021-06-prepared.csv
+tail -n +2 /docker-entrypoint-initdb.d/2021-07.csv | tac > /docker-entrypoint-initdb.d/2021-07-prepared.csv
+
--- backend/db/2_dataImport.sql
+++ backend/db/2_dataImport.sql
@@ -3,24 +3,21 @@ INTO TABLE journeys
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
 (departure_date_time, return_date_time, departure_station_id, departure_station_name, return_station_id, return_station_name, @covered_distance_m, duration_sec)
 SET covered_distance_m = NULLIF(@covered_distance_m, '');
 
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/2021-06.csv'
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/2021-06-prepared.csv'
 INTO TABLE journeys
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
 (departure_date_time, return_date_time, departure_station_id, departure_station_name, return_station_id, return_station_name, @covered_distance_m, duration_sec)
 SET covered_distance_m = NULLIF(@covered_distance_m, '');
 
-LOAD DATA INFILE '/docker-entrypoint-initdb.d/2021-07.csv'
+LOAD DATA INFILE '/docker-entrypoint-initdb.d/2021-07-prepared.csv'
 INTO TABLE journeys
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
 (departure_date_time, return_date_time, departure_station_id, departure_station_name, return_station_id, return_station_name, @covered_distance_m, duration_sec)
 SET covered_distance_m = NULLIF(@covered_distance_m, '');
