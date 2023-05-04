3f98e12bc264e3630ce31598c0db56081d443df6
Ina <iandersin@gmail.com>
2023-05-04 14:05:11 +0300

    Created JourneyTab.css, added basic css to rough draft the layout

--- frontend/src/components/JourneysPage.tsx
+++ frontend/src/components/JourneysPage.tsx
@@ -1,21 +1,31 @@
 import React from "react";
 import useDatabase from "../hooks/useDatabase";
 
-export default function JourneysPage(){
+export default function JourneysPage() {
     const {journeys} = useDatabase();
 
     return (
-        <div className="journey--container">
+        <div className="journey-tab--container">
+            <div className="journey-tab--header">
+                <div>Departure Date</div>
+                <div>Return Date</div>
+                <div>Departure Station Id</div>
+                <div>Departure Station Name</div>
+                <div>Return Station Id</div>
+                <div>Return Station Name</div>
+                <div>Covered Distance (m)</div>
+                <div>Duration (sec)</div>
+            </div>
             {journeys.map(journey => (
-                <div className="journey--wrapper">
-                    <div className="journey--wrapper-data">{journey.departure_date_time}</div>
-                    <div className="journey--wrapper-data">{journey.return_date_time}</div>
-                    <div className="journey--wrapper-data">{journey.departure_station_id}</div>
-                    <div className="journey--wrapper-data">{journey.departure_station_name}</div>
-                    <div className="journey--wrapper-data">{journey.return_station_id}</div>
-                    <div className="journey--wrapper-data">{journey.return_station_name}</div>
-                    <div className="journey--wrapper-data">{journey.covered_distance_m}</div>
-                    <div className="journey--wrapper-data">{journey.duration_sec}</div>
+                <div className="journey-tab--journey-list">
+                    <div className="journey-tab--journey-data">{journey.departure_date_time}</div>
+                    <div className="journey-tab--journey-data">{journey.return_date_time}</div>
+                    <div className="journey-tab--journey-data">{journey.departure_station_id}</div>
+                    <div className="journey-tab--journey-data">{journey.departure_station_name}</div>
+                    <div className="journey-tab--journey-data">{journey.return_station_id}</div>
+                    <div className="journey-tab--journey-data">{journey.return_station_name}</div>
+                    <div className="journey-tab--journey-data">{journey.covered_distance_m}</div>
+                    <div className="journey-tab--journey-data">{journey.duration_sec}</div>
                 </div>
             ))}
         </div>
