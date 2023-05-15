091d97e4d1c30b96020a8e2cb1bf4783375b2c7d
Ina <iandersin@gmail.com>
2023-05-15 14:44:57 +0300

    Changed journey div to table, deleted unused imports and state

--- frontend/src/components/JourneysPage.tsx
+++ frontend/src/components/JourneysPage.tsx
@@ -1,47 +1,54 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import useJourney from "../hooks/useJourney";
-import {Button} from "react-bootstrap";
+import {Button, Table} from "react-bootstrap";
 import LoadingSpinner from "./LoadingSpinner";
-import { v4 as uuidv4 } from 'uuid';
-import { format } from 'date-fns'
+import {v4 as uuidv4} from 'uuid';
+import {format} from 'date-fns'
 
 export default function JourneysPage() {
 
     const {journeys, getJourneys, showSpinner} = useJourney();
-    const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
 
-    useEffect(()=> {
+    useEffect(() => {
         getJourneys(1);
     }, []);
 
     return (
         <div className="journey-tab--container">
-            <div className="journey-tab--header">
-                <div>Departure Date</div>
-                <div>Return Date</div>
-                <div>Departure Station Id</div>
-                <div>Departure Station Name</div>
-                <div>Return Station Id</div>
-                <div>Return Station Name</div>
-                <div>Covered Distance (km)</div>
-                <div>Duration (min)</div>
-            </div>
-            {showSpinner && <LoadingSpinner/>}
-            {!showSpinner && journeys.content.map(journey => (
-                <div key={uuidv4()} className="journey-tab--journey-list">
-                    <div className="journey-tab--journey-data">{format(new Date(journey.departure_date_time), 'dd.MM.yyyy')}</div>
-                    <div className="journey-tab--journey-data">{format(new Date(journey.return_date_time), 'dd.MM.yyyy')}</div>
-                    <div className="journey-tab--journey-data">{journey.departure_station_id}</div>
-                    <div className="journey-tab--journey-data">{journey.departure_station_name}</div>
-                    <div className="journey-tab--journey-data">{journey.return_station_id}</div>
-                    <div className="journey-tab--journey-data">{journey.return_station_name}</div>
-                    <div className="journey-tab--journey-data">{(journey.covered_distance_m/1000).toFixed(1)}</div>
-                    <div className="journey-tab--journey-data">{(journey.duration_sec/60).toFixed(2)}</div>
-                </div>
-            ))}
+            <Table striped bordered hover>
+                <thead>
+                <tr>
+                    <th>Departure Date</th>
+                    <th>Return Date</th>
+                    <th>Departure Station Id</th>
+                    <th>Departure Station Name</th>
+                    <th>Return Station Id</th>
+                    <th>Return Station Name</th>
+                    <th>Covered Distance (km)</th>
+                    <th>Duration (min)</th>
+                </tr>
+                </thead>
+                <tbody>
+                {showSpinner && <LoadingSpinner/>}
+                {!showSpinner && journeys.content.map(journey => (
+                    <tr key={uuidv4()}>
+                        <td>{format(new Date(journey.departure_date_time), 'dd.MM.yyyy')}</td>
+                        <td>{format(new Date(journey.return_date_time), 'dd.MM.yyyy')}</td>
+                        <td>{journey.departure_station_id}</td>
+                        <td>{journey.departure_station_name}</td>
+                        <td>{journey.return_station_id}</td>
+                        <td>{journey.return_station_name}</td>
+                        <td>{(journey.covered_distance_m / 1000).toFixed(1)}</td>
+                        <td>{(journey.duration_sec / 60).toFixed(2)}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </Table>
             <div className="journey-tab--buttons">
-                <Button variant="outline-dark" disabled={!journeys.prev} onClick={()=> getJourneys(journeys.prevPageId)}>Prev</Button>
-                <Button variant="outline-dark" disabled={!journeys.next} onClick={()=> getJourneys(journeys.nextPageId)}>Next</Button>
+                <Button variant="outline-dark" disabled={!journeys.prev}
+                        onClick={() => getJourneys(journeys.prevPageId)}>Prev</Button>
+                <Button variant="outline-dark" disabled={!journeys.next}
+                        onClick={() => getJourneys(journeys.nextPageId)}>Next</Button>
             </div>
         </div>
     );
