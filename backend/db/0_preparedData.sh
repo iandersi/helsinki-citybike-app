dffd30ac41a14dcad4db6ab4c7938fa987da8ccb
Ina <iandersin@gmail.com>
2023-05-26 12:48:53 +0300

    Set lf endings on sh files

--- backend/db/0_preparedData.sh
+++ backend/db/0_preparedData.sh
@@ -1,4 +1,3 @@
 tail -n +2 /docker-entrypoint-initdb.d/2021-05.csv | tac > /docker-entrypoint-initdb.d/2021-05-prepared.csv
 tail -n +2 /docker-entrypoint-initdb.d/2021-06.csv | tac > /docker-entrypoint-initdb.d/2021-06-prepared.csv
 tail -n +2 /docker-entrypoint-initdb.d/2021-07.csv | tac > /docker-entrypoint-initdb.d/2021-07-prepared.csv
-
